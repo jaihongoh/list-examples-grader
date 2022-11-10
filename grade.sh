45a523fedb9819676d43b8d806cacb604872b28f
jaihongoh <114672327+jaihongoh@users.noreply.github.com>
2022-11-11 05:35:55 +0900

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -1,7 +1,10 @@
 # Create your grading script here
 
 set -e
+CPATH = .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar
 
 rm -rf student-submission
 git clone $1 student-submission
+
+cp ListExamples.java
 
